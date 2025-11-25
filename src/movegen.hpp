492c7ab028d17306790baf004a66b19359479eef
jxu <7989982+jxu@users.noreply.github.com>
2025-11-25 18:46:24 -0500

    Add start of movegen

---
+++ src/movegen.hpp
@@ -0,0 +1,26 @@
+#include "move.hpp"
+#include <vector>
+
+enum Direction
+{
+    NN = 16,
+    EE = 1,
+    SS = -16,
+    WW = -1,
+    NE = NN + EE,
+    SE = SS + EE,
+    SW = SS + WW,
+    NW = NN + WW,
+};
+
+constexpr std::array<int, 4> DIRECTION_ROOK = {NN, EE, SS, WW};
+constexpr std::array<int, 4> DIRECTION_BISHOP = {NE, SE, SW, NW};
+constexpr std::array<int, 8> DIRECTION_QUEEN = {NN, EE, SS, WW, NE, SE, SW, NW};
+
+// to optimize later to fixed size
+// gets piece type and color from position
+inline std::vector<Move> generate_slider(const Position& pos, const square sq)
+{
+
+};
+
